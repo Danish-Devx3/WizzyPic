5cd682a3988dbbf6e38bdf8667d50d9f24380358
Danish-Devx3 <danish.devx3@gmail.com>
2025-05-27 11:44:07 +0530

    feat: enhance image upload component and add particle effects to the application

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import { useState } from 'react'
 
 import Home from './components/Home'
 
+import './App.css'
+
 function App() {
+
+  
+  
   
 
   return (
     <div >
+      
       <Home />
+    
     </div>
   )
 }
--- src/components/ImgUpload.jsx
+++ src/components/ImgUpload.jsx
@@ -22,7 +22,7 @@ const ImgUpload = (props) => {
                 className="w-full h-full object-cover rounded-md"
               ></video>
             </div>
-            <h2 className="font-bold text-4xl mt-6 leading-12">
+            <h2 className="text-amber-300 font-bold text-4xl mt-6 leading-12">
               Enhance Your Image Quality Instantly with Free AI Tools
             </h2>
             <p className="leading-5 mt-6">
@@ -33,7 +33,7 @@ const ImgUpload = (props) => {
 
           <div className="flex items-center justify-center h-full w-full">
             <label
-              for="dropzone-file"
+              htmlFor="dropzone-file"
               className="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-300"
             >
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -46,14 +46,14 @@ const ImgUpload = (props) => {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                   />
                 </svg>
                 <p className="mb-2 text-sm ] ">
-                  <span class="font-semibold">Click to upload</span> or drag and
+                  <span className="font-semibold">Click to upload</span> or drag and
                   drop
                 </p>
                 <p className="text-xs">
